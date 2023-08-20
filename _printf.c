1e69775a1c831e50345f2b6b2a33310ffc538b4c
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-20 13:23:02 -0700

    tast 0

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _printf - produces output according to a format.
+ * @format: character string.
+ * Return: the number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int c = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, foramt, 1);
+		}
+		format++;
+	}
+	return (c);
+}
